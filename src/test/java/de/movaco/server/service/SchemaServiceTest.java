5444482d71ae829af38f7b688d9a89ec8c24c343
Florian Muth <43805040+htmoia@users.noreply.github.com>
2020-03-24 16:57:40 +0100

    Fix tests by removing wrong annotations

--- src/test/java/de/movaco/server/service/SchemaServiceTest.java
+++ src/test/java/de/movaco/server/service/SchemaServiceTest.java
@@ -3,11 +3,8 @@ package de.movaco.server.service;
 import de.movaco.server.shared.TenantTestBase;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 class SchemaServiceTest extends TenantTestBase {
-
-  @Autowired private SchemaService schemaService;
 
   @Test
   public void listSchemas() {
--- src/test/java/de/movaco/server/shared/TenantTestBase.java
+++ src/test/java/de/movaco/server/shared/TenantTestBase.java
@@ -12,13 +12,10 @@ import de.movaco.server.service.SchemaService;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public abstract class TenantTestBase {
 
