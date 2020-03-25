5f64461590dc1e643fc843bd17ee5889a6f67e1e
Florian Muth <43805040+htmoia@users.noreply.github.com>
2020-03-25 17:15:22 +0100

    Move JWTProcessor as inner class to CognitoJWTProcessor

--- src/main/java/de/movaco/server/security/cognito/CognitoJWTProcessor.java
+++ src/main/java/de/movaco/server/security/cognito/CognitoJWTProcessor.java
@@ -1,9 +1,20 @@
 package de.movaco.server.security.cognito;
 
+import static com.nimbusds.jose.JWSAlgorithm.RS256;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SimpleSecurityContext;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import de.movaco.server.exception.JwtParsingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -35,6 +46,30 @@ public class CognitoJWTProcessor {
       return new URL(url);
     } catch (MalformedURLException e) {
       throw new JwtParsingException("Could not build proper jwk URL object from " + url, e);
+    }
+  }
+
+  private class JWTProcessor extends DefaultJWTProcessor<SimpleSecurityContext> {
+
+    private final SimpleSecurityContext securityContext;
+
+    private JWTProcessor(JWKSource<SimpleSecurityContext> keySource) {
+      this.securityContext = new SimpleSecurityContext();
+      JWSKeySelector<SimpleSecurityContext> keySelector =
+          new JWSVerificationKeySelector<>(RS256, keySource);
+      super.setJWSKeySelector(keySelector);
+    }
+
+    JWTProcessor(URL jwkUrl) {
+      this(new RemoteJWKSet<>(jwkUrl, null));
+    }
+
+    JWTClaimsSet getClaims(String jwt) {
+      try {
+        return this.process(jwt, this.securityContext);
+      } catch (ParseException | BadJOSEException | JOSEException e) {
+        throw new JwtParsingException("Could not parse " + jwt, e);
+      }
     }
   }
 }
